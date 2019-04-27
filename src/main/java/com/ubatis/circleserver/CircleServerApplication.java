633b1d4504685596822075ec25089482c4ed62d8
Cong <809556311@qq.com>
2019-04-27 18:46:47 +0800

    first commit

---
+++ src/main/java/com/ubatis/circleserver/CircleServerApplication.java
@@ -0,0 +1,13 @@
+package com.ubatis.circleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleServerApplication.class, args);
+	}
+
+}
